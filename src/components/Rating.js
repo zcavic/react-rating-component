cfe1b049baf8eb32f2da750f6377296891cd5b32
Zeljko Cavic <zeljkocavic@rivian.com>
2023-08-02 17:50:58 +0200

    Refactoring

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -5,11 +5,13 @@ const Rating = (props) => {
   const emptyIcon = "/icons/stars/empty.svg";
   const filledIcon = "/icons/stars/filled.svg";
   const halfFilledIcon = "/icons/stars/half.svg";
-  const [icon1, setIcon1] = useState(emptyIcon);
-  const [icon2, setIcon2] = useState(emptyIcon);
-  const [icon3, setIcon3] = useState(emptyIcon);
-  const [icon4, setIcon4] = useState(emptyIcon);
-  const [icon5, setIcon5] = useState(emptyIcon);
+  const [icons, setIcons] = useState([
+    emptyIcon,
+    emptyIcon,
+    emptyIcon,
+    emptyIcon,
+    emptyIcon,
+  ]);
   const [selectedRating, setSelectedRating] = useState(0);
 
   // Utility function to calculate if the mouse event happened on the left side of the target or the right side.
@@ -22,147 +24,46 @@ const Rating = (props) => {
   };
 
   const onMouseOver = (event) => {
-    setIcon1(emptyIcon);
-    setIcon2(emptyIcon);
-    setIcon3(emptyIcon);
-    setIcon4(emptyIcon);
-    setIcon5(emptyIcon);
-    switch (event.currentTarget.id) {
-      case "1":
-        if (isLessThanHalf(event)) setIcon1(halfFilledIcon);
-        else setIcon1(filledIcon);
-        break;
-      case "2":
-        setIcon1(filledIcon);
-        if (isLessThanHalf(event)) setIcon2(halfFilledIcon);
-        else setIcon2(filledIcon);
-        break;
-      case "3":
-        setIcon1(filledIcon);
-        setIcon2(filledIcon);
-        if (isLessThanHalf(event)) setIcon3(halfFilledIcon);
-        else setIcon3(filledIcon);
-        break;
-      case "4":
-        setIcon1(filledIcon);
-        setIcon2(filledIcon);
-        setIcon3(filledIcon);
-        if (isLessThanHalf(event)) setIcon4(halfFilledIcon);
-        else setIcon4(filledIcon);
-        break;
-      case "5":
-        setIcon1(filledIcon);
-        setIcon2(filledIcon);
-        setIcon3(filledIcon);
-        setIcon4(filledIcon);
-        if (isLessThanHalf(event)) setIcon5(halfFilledIcon);
-        else setIcon5(filledIcon);
-        break;
-      default:
-        console.log(`Invalid ID.`);
+    let selected = Number(event.currentTarget.id);
+    if (isLessThanHalf(event)) selected += 0.5;
+    else selected += 1;
+
+    const newIcons = new Array(5).fill(emptyIcon);
+    for (let i = 0; i < 5; i++) {
+      if (i < selected) newIcons[i] = filledIcon;
+      if (i + 0.5 === selected) newIcons[i] = halfFilledIcon;
     }
+    setIcons(newIcons);
   };
 
   const onClick = (event) => {
     let half = 0;
     if (isLessThanHalf(event)) half = 0.5;
-    setSelectedRating(Number(event.currentTarget.id) - half);
+    setSelectedRating(Number(event.currentTarget.id) - half + 1);
   };
 
   const onMouseLeave = () => {
-    setIcon1(emptyIcon);
-    setIcon2(emptyIcon);
-    setIcon3(emptyIcon);
-    setIcon4(emptyIcon);
-    setIcon5(emptyIcon);
-    if (selectedRating > 4) {
-      setIcon1(filledIcon);
-      setIcon2(filledIcon);
-      setIcon3(filledIcon);
-      setIcon4(filledIcon);
-      if (selectedRating === 5) setIcon5(filledIcon);
-      else setIcon5(halfFilledIcon);
-    } else if (selectedRating > 3) {
-      setIcon1(filledIcon);
-      setIcon2(filledIcon);
-      setIcon3(filledIcon);
-      if (selectedRating === 4) setIcon4(filledIcon);
-      else setIcon4(halfFilledIcon);
-    } else if (selectedRating > 2) {
-      setIcon1(filledIcon);
-      setIcon2(filledIcon);
-      if (selectedRating === 3) setIcon3(filledIcon);
-      else setIcon3(halfFilledIcon);
-    } else if (selectedRating > 1) {
-      setIcon1(filledIcon);
-      if (selectedRating === 2) setIcon2(filledIcon);
-      else setIcon2(halfFilledIcon);
-    } else if (selectedRating > 0) {
-      if (selectedRating === 1) setIcon1(filledIcon);
-      else setIcon1(halfFilledIcon);
-    } else {
-      setIcon1(emptyIcon);
-      setIcon2(emptyIcon);
-      setIcon3(emptyIcon);
-      setIcon4(emptyIcon);
-      setIcon5(emptyIcon);
+    const newIcons = new Array(5).fill(emptyIcon);
+    for (let i = 0; i < 5; i++) {
+      if (i < selectedRating) newIcons[i] = filledIcon;
+      if (i + 0.5 === selectedRating) newIcons[i] = halfFilledIcon;
     }
+    setIcons(newIcons);
   };
 
-  const renderSymbol = () => {
+  const renderSymbol = (index) => {
     return (
-      <div>
-        <img
-          id="1"
-          src={icon1}
-          className="rating-image"
-          data-testid="rating-icon"
-          alt="Rate"
-          onMouseMove={onMouseOver}
-          onMouseLeave={onMouseLeave}
-          onClick={onClick}
-        />
-        <img
-          id="2"
-          src={icon2}
-          className="rating-image"
-          data-testid="rating-icon"
-          alt="Rate"
-          onMouseMove={onMouseOver}
-          onMouseLeave={onMouseLeave}
-          onClick={onClick}
-        />
-        <img
-          id="3"
-          src={icon3}
-          className="rating-image"
-          data-testid="rating-icon"
-          alt="Rate"
-          onMouseMove={onMouseOver}
-          onMouseLeave={onMouseLeave}
-          onClick={onClick}
-        />
-        <img
-          id="4"
-          src={icon4}
-          className="rating-image"
-          data-testid="rating-icon"
-          alt="Rate"
-          onMouseMove={onMouseOver}
-          onMouseLeave={onMouseLeave}
-          onClick={onClick}
-        />
-        <img
-          id="5"
-          src={icon5}
-          className="rating-image"
-          data-testid="rating-icon"
-          alt="Rate"
-          onMouseMove={onMouseOver}
-          onMouseLeave={onMouseLeave}
-          onClick={onClick}
-        />
-      </div>
+      <img
+        id={index}
+        key={index}
+        src={icons[index]}
+        className="rating-image"
+        data-testid="rating-icon"
+        alt="Rate"
+        onMouseMove={onMouseOver}
+        onMouseLeave={onMouseLeave}
+        onClick={onClick}
+      />
     );
   };
 
@@ -172,7 +73,7 @@ const Rating = (props) => {
       className="star-rating"
       data-testid="star-rating-container"
     >
-      {renderSymbol()}
+      {new Array(5).fill("").map((vale, index) => renderSymbol(index))}
     </div>
   );
 };
